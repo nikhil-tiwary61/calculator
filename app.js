bd132a4a9688ea190520a2941fe5f11b1339131d
Nikhil Tiwary <ntiwary61@gmail.com>
2023-05-25 22:37:21 +0530

    Feat: Add functions for add, stbtract, multiply, divide

---
+++ app.js
@@ -0,0 +1,12 @@
+const addNumber = (firstNumber, secondNumber) => {
+  return firstNumber + secondNumber;
+};
+const subtractNumber = (firstNumber, secondNumber) => {
+  return firstNumber - secondNumber;
+};
+const multiplyNumber = (firstNumber, secondNumber) => {
+  return firstNumber * secondNumber;
+};
+const divideNumber = (firstNumber, secondNumber) => {
+  return firstNumber / secondNumber;
+};
