e194cec9d758abf2a6704e223b7e949e00983569
Nikhil Tiwary <ntiwary61@gmail.com>
2023-05-30 21:54:43 +0530

    Add basic front-end

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ const multiplyNumber = (firstNumber, secondNumber) => {
 const divideNumber = (firstNumber, secondNumber) => {
   return firstNumber / secondNumber;
 };
+
+const displayCharacter = (button) => {
+  const displayExpression = document.getElementById("display");
+  console.log(displayExpression);
+};
+
+// const addBtn = document.getElementById("add-btn");
+// const subtractBtn = document.getElementById("add-btn");
+// const multiplyBtn = document.getElementById("add-btn");
+// const divideBtn = document.getElementById("add-btn");
+
+function calculator() {
+  const buttons = document.querySelectorAll("button");
+  Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", displayCharacter(button));
+  });
+}
