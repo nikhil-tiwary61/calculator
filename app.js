0b7350c3e7286d0d13d5557fbfe792f9fd483482
Nikhil Tiwary <ntiwary61@gmail.com>
2023-05-31 12:11:12 +0530

    Add clear function

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.previousOperand = "";
+    this.currentOperand = "";
+    this.operation = undefined;
   }
 
   appendNumber(number) {}
